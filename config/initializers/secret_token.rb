885d0fced74b4cb1c2da0c67374214112d64158f
Jesus <jeketchuz4@gmail.com>
2014-04-02 17:33:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecoas::Application.config.secret_key_base = '16ee513d974780605b87e4e06102a3d4e3d08d23e1c90d1379e71dfc562c08e8df088bec7e95229adf39b6155cd9196c4c99e22efe3a145ad0ba6cf61ad1e20c'
